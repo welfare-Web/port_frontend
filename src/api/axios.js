d2ab90c808570089295b53ce7c9cddb9cac9f403
welfare-Web <tlepubwelfare@gmail.com>
2026-01-03 16:15:27 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://port-backend-5np3.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
